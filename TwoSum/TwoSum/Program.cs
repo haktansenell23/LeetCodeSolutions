460093d9b5dc6a3951e77babe3c836caf704c11e
Haktan Emre Senel <haktansenel23@gmail.com>
2024-08-24 00:35:10 +0300

    Two Sum leet code solution solved

---
+++ TwoSum/TwoSum/Program.cs
@@ -0,0 +1,33 @@
+﻿
+int[] TwoSum(int[] nums, int target)
+{
+    for (int i = 0; i < nums.Length; i++)
+    {
+
+        for (int j = i + 1; j<nums.Length; j++)
+        {
+            if (nums[i] + nums[j] == target)
+            {
+
+                return new int[] {i,j};
+            }
+        }
+
+    }
+
+    return new int[] {};
+}
+
+
+
+
+int[] nums = new int[4] { 2, 7, 11, 15 };
+
+
+Console.WriteLine($"{TwoSum(nums, 9)[0]},{TwoSum(nums, 9)[1]}"); // [0, 1]   
+
+
+
+
+
+
