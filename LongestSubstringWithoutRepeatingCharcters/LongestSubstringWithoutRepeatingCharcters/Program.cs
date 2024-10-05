fb7bb45e0aeecbc3dee02bac8d8aa2c4ba0426c3
Haktan Emre Senel <haktansenel23@gmail.com>
2024-10-05 20:59:48 +0300

    LengthOfLongestSubstring leet code problem solved

---
+++ LongestSubstringWithoutRepeatingCharcters/LongestSubstringWithoutRepeatingCharcters/Program.cs
@@ -0,0 +1,54 @@
+﻿ int LengthOfLongestSubstring(string s)
+{
+    Dictionary<string, int> dict = new Dictionary<string, int>();
+
+    string subStringTemp = "";
+
+  
+    //anviaj
+    for (int i = 0; i < s.Length; i++)
+    {
+        subStringTemp = s[i].ToString();
+        if (s.Length == 1)
+        {
+            return 1;
+        }
+        for (int j = i + 1; j < s.Length; j++)
+         {
+            if (!subStringTemp.Contains(s[j]))
+            {
+                subStringTemp = subStringTemp + s[j];
+            }
+
+            else
+            {
+                if (!dict.ContainsKey(subStringTemp))
+                {
+                    dict.Add(subStringTemp, subStringTemp.Length);
+
+                }
+                subStringTemp = "";
+                break;
+            }
+
+            if (j==s.Length-1 && !dict.Keys.Contains(subStringTemp))
+            {
+                dict.Add(subStringTemp, subStringTemp.Length);
+                subStringTemp = "";
+
+            }
+
+
+        }
+   
+    }
+    var result = dict.OrderByDescending(x => x.Value).FirstOrDefault().Value;
+
+
+    return result;
+}
+
+Console.WriteLine("Determine string : ");
+string tryingValue = Console.ReadLine();
+Console.WriteLine($"Result : {LengthOfLongestSubstring(tryingValue)}");
+Console.ReadLine();
